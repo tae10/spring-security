47030f5526f8e13dbc31ee5dfc905f10c906fec8
syuka <syuka.gmail>
2023-06-08 11:18:26 +0900

    feat:jdbc

--- src/main/java/org/edupoll/config/SecurityConfig.java
+++ src/main/java/org/edupoll/config/SecurityConfig.java
@@ -1,51 +1,61 @@
 package org.edupoll.config;
 
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
-
+	
+	
+	@Bean
+	public UserDetailsService users(DataSource dataSource) {
+		var userDetailsService = new JdbcUserDetailsManager(dataSource);
+		/*
+		 * 디폴트 스키마와 테이블 구성이 다르면 query 를 바꿔주면 됨.
+		 */
+		userDetailsService.setUsersByUsernameQuery("SELECT ID, PASSWORD, ENABLED FROM USERS WHERE ID=?");
+		userDetailsService.setAuthoritiesByUsernameQuery("SELECT USER_ID, ROLE FROM USER_ROLES WHERE USER_ID=?");
+		return userDetailsService;
+	}
+	
+	
+	/*
 	@Bean
 	public UserDetailsService users() {
+		//
 		UserDetails u1 = User.builder().username("aaaa").password("{noop}1111").roles("user").build();
 		UserDetails u2 = User.builder().username("bbbb").password("{noop}1111").roles("user", "manager").build();
-		UserDetails u3 = User.builder().username("cccc").password("{noop}1111").roles("user", "manager", "king")
+		UserDetails u3 = User.builder().username("cccc").password("{noop}1111").roles("user", "manager", "admin")
 				.build();
-		// 임의의유저 추가 role 을 user,manager,admin 이거 세게 부여
-
-		return new InMemoryUserDetailsManager(u1, u2, u3);
+		// 임의의유저 추가 . role을 user, manager, admin 이거 세개 부여
+		var userDetailsService =  new InMemoryUserDetailsManager(u1, u2, u3);
+		return userDetailsService;
 	}
+	*/
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
-		/*
-		 * http .csrf(t -> t.disable()) .authorizeHttpRequests(t -> t
-		 * .requestMatchers("/login", "/login-task").permitAll()
-		 * .requestMatchers("/admin/**").hasAuthority("ROLE_admin")
-		 * .requestMatchers("/manager/**").hasRole("manager")
-		 * .anyRequest().authenticated() )
-		 */
-		
-		http.csrf().disable(); // 토큰검증 안한다는거
-		http.authorizeHttpRequests(t -> t.requestMatchers("/auth", "/login").permitAll());
-		http.authorizeHttpRequests(t -> t.requestMatchers("/admin/**").hasAuthority("ROLE_king"));
-		http.authorizeHttpRequests(t -> t.requestMatchers("/manager/**").hasAnyRole("manager"));
-
-		// 모은요청은 인증을 받아야 처리된다, 기본설정한다고생각하셈 authenticated 말고 permitAll 하면 인증안되고 다들어가짐
-		http.authorizeHttpRequests(t -> t.anyRequest().authenticated());
-
-		// /auth 라는 로그인페이지 경로만 허락해준거
-		http.formLogin(form -> form.loginPage("/auth").loginProcessingUrl("/login"));
-
-		return http.build();
-	}
-
+	
+	
+	
+	
+	
+	
+	/* 
+	 * @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
+	 * Exception { http .csrf(t -> t.disable()) .authorizeHttpRequests(t -> t
+	 * .requestMatchers("/login", "/login-task").permitAll()
+	 * .requestMatchers("/admin/**").hasAuthority("ROLE_admin")
+	 * .requestMatchers("/manager/**").hasRole("manager")
+	 * .requestMatchers("/manager/**").hasRole("manager")
+	 * .anyRequest().authenticated()
+	 * 
+	 * ) .formLogin( form ->
+	 * form.loginPage("/login").loginProcessingUrl("/login-task") ); return
+	 * http.build(); }
+	 */
 }
--- src/main/java/org/edupoll/controller/IndexController.java
+++ src/main/java/org/edupoll/controller/IndexController.java
@@ -1,16 +1,42 @@
 package org.edupoll.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
 
 	@GetMapping("/")
-	public String indexHandle(Model model) {
+	public String indexHandle(HttpSession session,Model model) {
+	Authentication auth =	SecurityContextHolder.getContext().getAuthentication();
+	//인증을 안받으면 null 이 뜨긴함
+	UserDetails userDetails = (UserDetails)auth.getPrincipal();
+	System.out.println(userDetails);
 		model.addAttribute("greet", "힘쎄고 강한아침");
-
+		System.out.println(session.getAttribute("SPRING_SECURITY_CONTEXT"));
 		return "index";
 	}
+	@GetMapping("/status")
+	@ResponseBody
+	public Object statusHandle(HttpSession session) {
+		return session.getAttribute("SPRING_SECURITY_CONTEXT");
+	}
+	
+	@GetMapping("/my-home")
+	public String  showMyHome(@AuthenticationPrincipal UserDetails userDetails,Model model) {
+		
+		System.out.println(userDetails.getUsername());
+		model.addAttribute("greet", "힘쎄고 왈도");
+		
+		return "index";
+	}
+	
 }
